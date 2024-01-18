4ef2d5a223e5f2735592fcc4380acfeeb7af6123
iulica04 <126086021+iulica04@users.noreply.github.com>
2024-01-18 09:43:17 +0200

    Update IdList.h

--- IdList.h
+++ IdList.h
@@ -14,7 +14,7 @@ using namespace std;
 struct IdInfo {
     string type;
     string name;
-    string value; // Adăugăm valoarea variabilei
+    string value; 
 };
 
 struct IdConstant {
@@ -24,10 +24,10 @@ struct IdConstant {
 };
 
 struct IdArray {
-    string name; // Numele array-ului
-    string type; // Tipul array-ului
-    int size;         // Dimensiunea array-ului
-    vector<std::string> values; // Valorile array-ului
+    string name; 
+    string type; 
+    int size;         
+    vector<std::string> values; 
 };
 
 
@@ -37,46 +37,70 @@ struct IdFunction {
     std::string parameters;
 };
 
+struct ClassMember {
+    std::string type;
+    std::string id;
+};
 
+struct ClassVariable {
+    std::string name;
+    std::vector<ClassMember> members;
+};
+
+struct IdClass {
+    string name;
+    vector<IdInfo> vars;
+    vector<IdArray> arrays;
+    vector<IdConstant> constants;
+    vector<IdFunction> functions;
+    vector<ClassVariable> classVariables;
+};
 
 class IdList {
 public:
     vector<IdInfo> vars;
     vector<IdArray> arrays;
-    std::vector<IdFunction> functions;
-    std::vector<IdConstant> constants;
+    vector<IdFunction> functions;
+    vector<IdConstant> constants;
+    vector<IdClass> classes;
 
 
-    bool existsVar(const char* s);
-    bool existsFunction(const char* s);
-    bool existsArray(const char* s);
-    bool existsConstant(const char* s);
+    bool existsVar(string s);
+    bool existsFunction(string s);
+    bool existsArray(string s);
+    bool existsConstant(string s);
+    bool existsClass(string s);
     void addVar(const char* type, const char* name);
     void addArray(const char* type, const char* name, int size);
     void addFunction(const std::string& type, const std::string& name, const std::string& parameters);
     void addConstant(const char* type, const char* name, const char* value);
+    void addClass(const string& name);
     void printVars();
     void printFunctions(); 
     void printConstants();
     void printArrays();
+    void printClasses();
     string getVarType(string var);
-    string getConstType(const char* var);
+    string getConstType(string var);
     string getArrayType(string var);
-    string getFunctionType(const char* var);
+    string getFunctionType(string var);
     string getVarValue(string var);
-    string getConstValue(const char* con);
+    string getConstValue(string con);
     string getArrayElementValue(string var, int index);
     void updateVarValue(const char* var, string value);
     void updateArrayValue(const char* name, int index, string value);
     int getArraySize(const char* name);
-    void incrementVar(const char* name);
-    void incrementArrayElement(const char* name, int index);
-    void decrementVar(const char* name);
-    void decrementArrayElement(const char* name, int index); 
+    void incrementVar(const char* name, int yylineno);
+    void incrementArrayElement(const char* name, int index, int yylineno);
+    void decrementVar(const char* name, int yylineno);
+    void decrementArrayElement(const char* name, int index, int yylineno); 
 
-    bool matchFunctionArguments(const char* name, string argumente);
+    bool matchFunctionArguments(const char* name, string argumente, int yylineno);
+
+    void addElementtoClass(const char* name, string argumente);
 
     ~IdList();
 };
 
-#endif // IDLIST_H
+#endif 
+
